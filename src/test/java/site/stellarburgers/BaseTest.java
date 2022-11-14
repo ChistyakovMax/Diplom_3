a0cec7b433d1b13526a764e9c2c4050f547b2fd6
Max Chistyakov <chistyakovmaxim2000@gmail.com>
2022-11-14 11:30:08 +0300

    Base test add

---
+++ src/test/java/site/stellarburgers/BaseTest.java
@@ -0,0 +1,25 @@
+package site.stellarburgers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//общий класс для тестов
+public abstract class BaseTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/site/stellarburgers/RegistrationTest.java
@@ -0,0 +1,8 @@
+package site.stellarburgers;
+
+import jdk.jfr.Description;
+
+@Description("Регистрация нового пользователя")
+public class RegistrationTest extends BaseTest{
+
+}
